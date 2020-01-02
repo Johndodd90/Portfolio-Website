7883ac3db568e58e0ef248830be60483ee5a9f6b
John Dodd <johndodd90@gmail.com>
2020-01-02 18:13:20 +0000

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER':'postgres',
-        'PASSWORD':'Word3288230',
+        'USER':'',
+        'PASSWORD':'',
         'HOST': 'localhost',
         'PORT': '5432',
     }
